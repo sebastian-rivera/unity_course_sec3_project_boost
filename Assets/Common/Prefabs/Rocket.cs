5f5a41108ca31098fbd1682a114453eebfd8b40d
Sebastian Rivera <cronorpg@hotmail.com>
2021-02-02 00:06:52 -0400

    Fixed issue with freeze rotation

--- Assets/Common/Prefabs/Rocket.cs
+++ Assets/Common/Prefabs/Rocket.cs
@@ -10,36 +10,46 @@ public class Rocket : MonoBehaviour {
 	// Start is called before the first frame update
 	void Start() {
 		_rigidBody = GetComponent<Rigidbody>();
+		//_rigidBody.constraints = RigidbodyConstraints.FreezeRotation;
 		_audioSource = GetComponent<AudioSource>();
 	}
 
 	// Update is called once per frame
 	void Update() {
-		if (Input.GetKey(KeyCode.Space)) {
-			_rigidBody.AddRelativeForce(Time.deltaTime * thrustSpeed * Vector3.up);
+		Thrust();
+		Rotate();
 
-			if(_audioSource.isPlaying == false) {
-				_audioSource.Play();
-			}
+	}
 
-		} else {
-			if(_audioSource.isPlaying) {
-				_audioSource.Stop();
-			}
-		}
+	private void Rotate() {
 
-		if(Input.GetKey(KeyCode.A)) {
+	
+
+		if (Input.GetKey(KeyCode.A)) {
 			transform.Rotate(Time.deltaTime * rotationSpeed * Vector3.forward);
 			_rigidBody.angularVelocity = Vector3.zero;
 		}
 
-		if(Input.GetKey(KeyCode.D)) {
+		if (Input.GetKey(KeyCode.D)) {
 			transform.Rotate(Time.deltaTime * rotationSpeed * -Vector3.forward);
 			_rigidBody.angularVelocity = Vector3.zero;
 		}
 
-		transform.rotation = new Quaternion(0, 0, transform.rotation.z, transform.rotation.w);
-		transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+		
+	}
 
+	private void Thrust() {
+		if (Input.GetKey(KeyCode.Space)) {
+			_rigidBody.AddRelativeForce(Time.deltaTime * thrustSpeed * Vector3.up);
+
+			if (_audioSource.isPlaying == false) {
+				_audioSource.Play();
+			}
+
+		} else {
+			if (_audioSource.isPlaying) {
+				_audioSource.Stop();
+			}
+		}
 	}
 }
